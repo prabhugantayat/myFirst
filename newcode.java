f4acb619a8bc5f157d56c51bd24722bd40870b7c
PG <prabhu.g@chaipoint.com>
2019-12-17 11:34:34 +0530

    code add chesa

---
+++ newcode.java
@@ -0,0 +1,13 @@
+class a{
+    //String s;
+    static void call(String s){
+        System.out.println(s);
+    }
+
+}
+class b{
+    public static void main(){
+        String s = "hey pillagada";
+        a.call(s);
+    }
+}
